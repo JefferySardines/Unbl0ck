4ebe5cfb6e14f4631f517373472f2a0ddf020cc9
JefferySardines <132076418+JefferySardines@users.noreply.github.com>
2023-04-28 14:44:08 +0000

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var secretCode = '';
-document.addEventListener('keydown', function(event) {
-  if (event.keyCode >= 49 && event.keyCode <= 53) {
-    secretCode += String.fromCharCode(event.keyCode);
-    if (secretCode === '12345') {
-      window.location.href = 'secret.html';
-    }
-  }
-});
